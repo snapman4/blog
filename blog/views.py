53963374e028d3270490b8dab0762951843f82d7
snapman4 <donaldpaulmunson@gmail.com>
2018-08-14 14:39:34 -0700

    fixed post_detail.html

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,7 @@ def post_list(request):
     posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
