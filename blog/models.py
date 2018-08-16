8ae4504219ab6f35e7ac8e6cbe434ddcb6a58568
snapman4 <donaldpaulmunson@gmail.com>
2018-08-16 13:32:47 -0700

    with url link and picture option

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,8 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     publish_date = models.DateTimeField(blank=True, null=True)
+    image = models.ImageField(upload_to="images", blank=True)
+    url = models.URLField(blank=True, null=True)
 
     def publish(self):
         self.publish_date = timezone.now()
--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('post/new', views.post_new, name='post_new'),
     path('post/<int:pk>/', views.post_edit, name='post_edit'),
 
-
 ]
 
